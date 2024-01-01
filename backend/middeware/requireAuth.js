5cd71ead009a73c00961c3cb04448dc24f680930
Weiwei <linweiwei2222@163.com>
2024-01-01 00:12:33 -0500

    practice

---
+++ backend/middeware/requireAuth.js
@@ -0,0 +1,24 @@
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const requireAuth = async (req, res, next) => {
+  // verify authentication
+  const { authoriztion } = req.headers;
+
+  if (!authoriztion) {
+    return res.status(401).json({ error: "Authorization token required" });
+  }
+
+  const token = authoriztion.split(" ")[1];
+
+  try {
+    const { _id } = jwt.verify(token, process.env.SECRET);
+    req.user = await User.findOne({ _id }).select("_id");
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ error: "Request is not authorized" });
+  }
+};
+
+module.exports = requireAuth;
