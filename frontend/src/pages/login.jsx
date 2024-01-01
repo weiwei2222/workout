771e30d9b4d5be54b25eb57e8f6965ef5863dd4e
Weiwei <linweiwei2222@163.com>
2023-12-31 23:38:09 -0500

    practice

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { useLogin } from "../hooks/useLogin";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login, isLoading, error } = useLogin();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(email, password);
+    await login(email, password);
   };
 
   return (
@@ -24,7 +26,8 @@ const Login = () => {
         onChange={(e) => setPassword(e.target.value)}
         value={password}
       ></input>
-      <button>Log in</button>
+      <button disabled={isLoading}>Log in</button>
+      {error && <div className="error">{error}</div>}
     </form>
   );
 };
